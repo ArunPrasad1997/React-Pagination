4c072f0d6667907f92b8c810f9377b029c42b3f9
arun@makolet.in <password9nine>
2023-09-01 12:19:32 +0530

    Indexjs fixed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import './index.css';
-import App from './App';
-import store from './store/redux-demo';
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./index.css";
+import App from "./App";
+// import store from './store/redux-demo';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Provider store={store}><App /></Provider>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+//   <Provider store={store}>
+    <App />
+//   </Provider>
+);
